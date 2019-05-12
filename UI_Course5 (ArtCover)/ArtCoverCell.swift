08fcb1cfecdecfb21c4354ecd9ba2ea072a2f6a7
Aleksandr Tarasov (exzanimo) <exzanimo@gmail.com>
2019-05-12 14:57:41 +0300

    Выполнено задание "15. UICollectionView" (без возможности тапа на плитки с композициями)

---
+++ UI_Course5 (ArtCover)/ArtCoverCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtCoverCell.swift
+//  UI_Course5 (ArtCover)
+//
+//  Created by Александр Тарасов on 12/05/2019.
+//  Copyright © 2019 Aleksandr Tarasov. All rights reserved.
+//
+
+import UIKit
+
+class ArtCoverCell: UICollectionViewCell {
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
